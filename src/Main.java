7491f163d58411262954e3abbc5eb55e4b30bfd7
const-test <kvshvarov@yandex.ru>
2022-06-15 20:27:25 +0300

    New

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 215;
+        int bonus = price / 20;
+
+        System.out.println("Ваши мили:" + bonus);
+    }
+}
